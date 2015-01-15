f435d63eb366ad102a8fd606c213d7d7f8ad5ed5
Josemar Ferreira <jf.sorin@gmail.com>
2015-01-15 09:28:56 -0200

    Complemento da Base
    Arquivos ignore

---
+++ frontend/config/main-local.php
@@ -0,0 +1,21 @@
+<?php
+
+$config = [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'cFHe0n8N67hzeuyoAg9abgY5K5MDxgO9',
+        ],
+    ],
+];
+
+if (!YII_ENV_TEST) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = 'yii\debug\Module';
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = 'yii\gii\Module';
+}
+
+return $config;
