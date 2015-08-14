c3902c34d875c47900b40aad56817f2b8a888ec1
Josemar Ferreira <jf.sorin@gmail.com>
2015-08-14 17:36:09 -0300

    Evento - HotEvent
    Ajustes

--- frontend/modules/hotEvent/views/default/index.php
+++ frontend/modules/hotEvent/views/default/index.php
@@ -2044,7 +2044,6 @@
 						</div>
 					</div>
 				</div>
-
 				<!-- -->
 				<hr class="page-divider line large"/>
 				<!-- -->
@@ -2053,11 +2052,11 @@
 					<div class="col-md-8 pull-left">
 						<h1 class="section-title">
 							<span class="icon-inner"><span class="fa-stack"><i class="fa rhex fa-stack-2x"></i><i class="fa fa-question fa-stack-1x"></i></span></span>
-							<span class="title-inner">Event FAQS <small> / find your answers</small></span>
+							<span class="title-inner">FAQ´s DO EVENTO <small> / encontre suas respostas</small></span>
 						</h1>
 					</div>
 					<div class="col-md-4 text-right-md pull-right">
-						<a href="#" class="btn btn-theme btn-theme-lg btn-theme-transparent-grey pull-right"><i class="fa fa-pencil"></i> Open a ticket</a>
+						<a href="#" class="btn btn-theme btn-theme-lg btn-theme-transparent-grey pull-right"><i class="fa fa-pencil"></i> AINDA ESTÁ COM DÚVIDAS ?</a>
 					</div>
 				</div>
 
@@ -2452,101 +2451,7 @@
 <!-- /Content area -->
 
 <!-- FOOTER -->
-<footer class="footer">
-	<div class="footer-widgets">
-		<div class="container">
-			<div class="row">
-
-				<div class="col-md-3">
-					<div class="widget widget-about">
-						<h4 class="widget-title">About Us</h4>
-						<p>Fusce pellentesque velvitae tincidunt egestas. Pellentesque habitant morbi.Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis.</p>
-						<address>
-							<div><i class="fa fa-phone"></i>+90 555 55 55</div>
-							<div><i class="fa fa-envelope"></i> <a href="mailto:info@example.com">info@example.com</a></div>
-						</address>
-					</div>
-				</div>
-				<div class="col-md-3">
-					<div class="widget widget-categories">
-						<h4 class="widget-title">Popular searches</h4>
-						<ul>
-						<li><a href="#">Online Registration</a></li>
-						<li><a href="#">Sell Event Tickets</a></li>
-						<li><a href="#">Post Events</a></li>
-						<li><a href="#">Event Planning Software</a></li>
-						<li><a href="#">Online Event Management</a></li>
-						<li><a href="#">Event Management Software</a></li>
-						<li><a href="#">Event Payment</a></li>
-						</ul>
-					</div>
-				</div>
-				<div class="col-md-3">
-					<div class="widget widget-twitter">
-						<h4 class="widget-title">Recent Tweets</h4>
-						<ul>
-							<li>
-								<a href="#">@isamercan</a> Cupcake chocolate cake sweet roll. Gummies macaroon biscuit cupcake candy dragée. <a href="#">#Conference about 2 hours ago</a>
-							</li>
-							<li>
-								<a href="#">@isamercan</a> Cupcake chocolate cake sweet roll. Gummies macaroon biscuit cupcake candy dragée. <a href="#">#Conference about 2 hours ago</a>
-							</li>
-						</ul>
-					</div>
-				</div>
-				<div class="col-md-3">
-					<div class="widget widget-flickr-feed">
-						<h4 class="widget-title"><span>Instagram Photos</span></h4>
-						<ul>
-							<li><a href="#"><img src="/gevents/frontend/web/hotevent/img/preview/sidebar-1.jpg" alt=""></a></li>
-							<li><a href="#"><img src="/gevents/frontend/web/hotevent/img/preview/sidebar-2.jpg" alt=""></a></li>
-							<li><a href="#"><img src="/gevents/frontend/web/hotevent/img/preview/sidebar-3.jpg" alt=""></a></li>
-							<li><a href="#"><img src="/gevents/frontend/web/hotevent/img/preview/sidebar-4.jpg" alt=""></a></li>
-							<li><a href="#"><img src="/gevents/frontend/web/hotevent/img/preview/sidebar-5.jpg" alt=""></a></li>
-							<li><a href="#"><img src="/gevents/frontend/web/hotevent/img/preview/sidebar-6.jpg" alt=""></a></li>
-							<!--li><a href="#"><img src="/gevents/frontend/web/hotevent/img/preview/sidebar-7.jpg" alt=""></a></li>
-							<li><a href="#"><img src="/gevents/frontend/web/hotevent/img/preview/sidebar-8.jpg" alt=""></a></li-->
-						</ul>
-					</div>
-				</div>
-
-			</div>
-		</div>
-	</div>
-	<div class="footer-meta footer-meta-alt">
-		<div class="container">
-
-			<div class="row">
-				<div class="col-md-9 col-sm-6">
-					<ul class="footer-menu">
-						<li><a href="#">About</a></li>
-						<li><a href="#">Blog</a></li>
-						<li><a href="#">Help</a></li>
-						<li><a href="#">Careers</a></li>
-						<li><a href="#">Press</a></li>
-						<li><a href="#">Developers</a></li>
-						<li><a href="#">Terms</a></li>
-						<li><a href="#">Privacy</a></li>
-						<li><a href="#">Cookies</a></li>
-						<li>All Rights Reserved</li>
-					</ul>
-				</div>
-				<div class="col-md-3 col-sm-6">
-					<form action="" class="country-select">
-						<div class="form-group">
-							<select class="selectpicker" data-width="100%" name="" id="">
-								<option>Select Your Country</option>
-								<option>Select Your Country</option>
-								<option>Select Your Country</option>
-							</select>
-						</div>
-					</form>
-				</div>
-			</div>
-
-		</div>
-	</div>
-</footer>
+<?= $this->render('/layouts/footer'); ?>
 <!-- /FOOTER -->
 
 <div class="to-top"><i class="fa fa-angle-up"></i></div>
