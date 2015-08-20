9a32a93260287d8ef0c7b3ba4c55f1900d2288f3
Bemerson Coutinho Lins <bemerson_lins@yahoo.com.br>
2015-08-20 00:20:43 -0300

    ...

--- frontend/modules/hotEvent/views/layouts/slider.php
+++
@@ -1,6 +0,0 @@
-<div class="owl-carousel img-carousel">
-	<div class="item"><img class="img-responsive" src="/gevents/frontend/web/hotevent/img/preview/img-slider-1.jpg" alt=""/></div>
-	<div class="item"><img class="img-responsive" src="/gevents/frontend/web/hotevent/img/preview/img-slider-1.jpg" alt=""/></div>
-	<div class="item"><img class="img-responsive" src="/gevents/frontend/web/hotevent/img/preview/img-slider-1.jpg" alt=""/></div>
-	<div class="item"><img class="img-responsive" src="/gevents/frontend/web/hotevent/img/preview/img-slider-1.jpg" alt=""/></div>
-</div>
